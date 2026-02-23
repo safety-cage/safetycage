31062f5cf66576b589f9ed77f25ce1d1bed41aad
juliaqiu <julia.qiu@sintef.no>
2026-02-23 15:03:26 +0100

    fi

--- examples/use_case/src/data/keras_cifar10_datahandler.py
+++ examples/use_case/src/data/keras_cifar10_datahandler.py
@@ -38,19 +38,19 @@ class KerasCIFAR10DataHandler(BaseDataHandler):
     @property
     def classes(self) -> Dict[int, str]:
         """Get the class names mapping."""
-        # return {
-        #     0: "airplane",
-        #     1: "automobile",
-        #     2: "bird",
-        #     3: "cat",
-        #     4: "deer",
-        #     5: "dog",
-        #     6: "frog",
-        #     7: "horse",
-        #     8: "ship",
-        #     9: "truck"
-        # }
-        return [i for i in range(10)]
+        return {
+            0: "airplane",
+            1: "automobile",
+            2: "bird",
+            3: "cat",
+            4: "deer",
+            5: "dog",
+            6: "frog",
+            7: "horse",
+            8: "ship",
+            9: "truck"
+        }
+        # return [i for i in range(10)]
     
     @property
     def num_classes(self) -> int:
